740dced48a799ce638f7738f6aa6f7cf69aa6329
Gabriel Stein <steindleve@gmail.com>
2018-07-04 16:00:26 -0400

    Craiglist offers load and display

--- src/App.js
+++ src/App.js
@@ -1,8 +1,61 @@
 import React, { Component } from 'react'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      loading: true,
+      craigListOffersList: []
+    }
+  }
+
+  componentDidMount() {
+    this.loadAndCreateCraiglistOffersList()
+  }
+
+  loadAndCreateCraiglistOffersList() {
+    const offersFile = require('./web-scraper/json-files/craiglist.json')
+    const offersList = offersFile.list
+
+    const craigListOffersList = []
+
+    offersList.map(offer => {
+      craigListOffersList.push(offer)
+    })
+
+    this.setState({
+      craigListOffersList,
+      loading: false
+    })
+  }
+
+  renderCraiglistOffers() {
+    const craigListOffersList = this.state.craigListOffersList
+    return (
+      <div>
+        {craigListOffersList.map(offer => {
+          return (
+            <div>
+              <div>
+                <div>{offer.title}</div>
+              </div>
+              <div>
+                <div>{offer.url}</div>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
+
   render() {
-    return <div>Hello world</div>
+    if (this.state.loading) {
+      return <div>Loading</div>
+    } else {
+      return this.renderCraiglistOffers()
+    }
   }
 }
 
